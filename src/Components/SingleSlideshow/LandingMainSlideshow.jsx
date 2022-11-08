a9a9560a1c1687a22cd550075513ff882939b907
subru-37 <easwaransubramaniOO7@gmail.com>
2022-11-08 14:57:17 +0530

    Added react-slideshow-image package

--- src/Components/SingleSlideshow/LandingMainSlideshow.jsx
+++ src/Components/SingleSlideshow/LandingMainSlideshow.jsx
@@ -1,13 +1,5 @@
 import React from "react";
 export default function LandingMainSlideshow() {
-    const settings = {
-      className: "center",
-      centerMode: true,
-      infinite: true,
-      centerPadding: "60px",
-      slidesToShow: 3,
-      speed: 500
-    };
     return (
       <div>
       sample
