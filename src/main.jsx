ce1e5c3e8134e7df79df188152042fd6d92d384a
subru-37 <easwaransubramaniOO7@gmail.com>
2022-11-09 20:38:45 +0530

    Changed document title name

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
 import App from './App'
 import './index.css'
-
+document.title = "Ace Bikes";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
